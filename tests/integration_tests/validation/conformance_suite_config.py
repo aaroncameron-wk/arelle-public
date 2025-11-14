54d58db561fc6bcee928e71b03df4a0ef8ef0dbe
Aaron Cameron <aaron.cameron@workiva.com>
2025-11-14 09:59:58 -0700

    Add ability for conformance suite configs to perform preprocessing

--- tests/integration_tests/validation/conformance_suite_config.py
+++ tests/integration_tests/validation/conformance_suite_config.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field
 from enum import Enum
 from functools import cached_property
 from pathlib import Path
-from typing import Literal, Any
+from typing import Literal, Any, Callable
 
 from tests.integration_tests.github import OS_CORES
 
@@ -212,6 +212,7 @@ class ConformanceSuiteConfig:
     expected_missing_testcases: frozenset[str] = frozenset()
     membership_url: str | None = None
     plugins: frozenset[str] = frozenset()
+    preprocessing_func: Callable[[ConformanceSuiteConfig], None] | None = None
     shards: int = 1
     strict_testcase_index: bool = True
     supports_test_engine: bool = True
