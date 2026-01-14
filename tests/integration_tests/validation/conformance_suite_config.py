c9c7ca52917b3394a19350c560eb3d98a5734418
Aaron Cameron <aaron.cameron@workiva.com>
2026-01-14 15:16:10 -0700

    Conformance suite configs run through Test Engine

--- tests/integration_tests/validation/conformance_suite_config.py
+++ tests/integration_tests/validation/conformance_suite_config.py
@@ -236,12 +236,6 @@ class ConformanceSuiteConfig:
             if p1.startswith(p2) or p2.startswith(p1)]
         assert not overlapping_prefixes, \
             f'Overlapping prefixes are not supported: {overlapping_prefixes}'
-        assert not (self.shards == 1 and self.additional_plugins_by_prefix), \
-            'Cannot specify additional_plugins_by_prefix with only one shard.'
-        plugin_combinations = len({plugins for _, plugins in self.additional_plugins_by_prefix}) + 1
-        assert plugin_combinations <= self.shards, \
-            'Too few shards to accommodate the number of plugin combinations:' \
-            f' combinations={plugin_combinations} shards={self.shards}'
         overlapping_expected_testcase_ids = self.expected_failure_ids.intersection(self.required_locale_by_ids)
         assert not overlapping_expected_testcase_ids, \
             f'Testcase IDs in both expected failures and required locales: {sorted(overlapping_expected_testcase_ids)}'
